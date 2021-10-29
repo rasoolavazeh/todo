e85549552fb145ab0be0f4805eb3846dec3524e9
Mohammad Rasool Avazeh <rasoolavazeh@yahoo.com>
2021-10-29 10:53:06 +0330

    add Dockerfile

--- backend/src/main/java/com/rasoolavazeh/todo/controllers/TodoController.java
+++ backend/src/main/java/com/rasoolavazeh/todo/controllers/TodoController.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/todos")
-@CrossOrigin(origins = "*git ")
+@CrossOrigin(origins = "*")
 public class TodoController {
 
     private final TodoRepository todoRepository;
